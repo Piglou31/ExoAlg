6b51fe771fbefeabb0987b16bc6cf7703aed264b
Piglou31 <tom.paletou@gmail.com>
2020-02-06 13:47:57 +0100

    Choix par numéro

---
+++ Explo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int destination;
+
+
+int main() {
+
+
+printf ("Ou voulez vous aller ? paradis(1) ou enfer(2)\n");
+scanf ("%d",&destination);
+
+	if ( destination == 1){
+	
+	printf ("Vous etes arrives en enfer");
+		}
+
+	if ( destination == 2){
+	printf ("Vous etes arrives au paradis");
+	
+		}
+
+
+
+
+
+
+return 0;
+}
