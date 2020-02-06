aaa29bdd48dd43d41ddd6dde993590a173531dc9
Piglou31 <tom.paletou@gmail.com>
2020-02-06 14:36:08 +0100

    gestion des lieux

--- Explo.c
+++ Explo.c
@@ -2,28 +2,31 @@
 #include <string.h>
 
 char destination[10];
-
+int choix = 1;
 
 int main() {
 
 
 printf ("Ou voulez vous aller ? paradis ou enfer\n");
+while ( (strcmp( destination,"paradis")<0 && strcmp( destination,"paradis")>0 && strcmp( destination,"enfer")<0 && strcmp(destination,"enfer")>0 || choix==1)) {
 scanf ("%s",&destination);
-
-	if ( strcmp( destination,"enfer")==0 ){
 	
+	if ( strcmp( destination,"enfer")==0 ){
 	printf ("Vous etes arrives en enfer allez vous faire\n");
+	choix = 0;
 		}
 
 	if (strcmp( destination,"paradis")==0 ){
 	printf ("Vous etes arrives au paradis");
+	choix = 0;
+		}
+	else  { 
+		printf ("Veuillez choisir une destination valide\n");
+
 	
 		}
-	else if (strcmp( destination,"paradis")<0 || strcmp( destination,"paradis")<0 ||strcmp( destination,"enfer")<0 || strcmp(destination,"enfer")>0){ 
-		printf ("Ou voulez vous aller ? paradis ou enfer\n");
-		scanf ("%s",&destination);
-		}
-
+		
+}
 
 
 return 0;
